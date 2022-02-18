de104f1b1038121e0dad1506b32eb382002bfb71
sharatsagar <93038979+sharatsagar@users.noreply.github.com>
2022-02-18 18:23:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function take_snapshot()
         document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
 } 
-console.log("ml5 version",ml5.version)
+
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7is23Ljp1/model.json',modelLoaded)
 function check(){
   img=document.getElementById("selfie_image")
